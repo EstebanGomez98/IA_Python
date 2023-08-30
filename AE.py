132fe16e51341cdda8cb3a228f3f729ffcbf65ac
EstebanGomez98 <43140553+EstebanGomez98@users.noreply.github.com>
2023-08-30 00:11:06 -0500

    se agrega el ejercicio de reconocimiento de las 5 vocales y re mejora el de reconocimiento de A y E

--- AE.py
+++ AE.py
@@ -1,39 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import tkinter as tk
 
-# Initializations
-w = np.random.rand(36)  # Initialize weights with random values between 0 and 1
+# Inicializaciones
+w = np.random.rand(36)  # Inicializar pesos con valores aleatorios entre 0 y 1
 x = np.array([
     [1, 98, 98, 98, 98, 98, 98, 255, 255, 255, 98, 98, 255, 255, 255, 98, 98, 98, 98, 98, 98, 98, 255, 255, 255, 98, 98, 255, 255, 255, 98, 98, 255, 255, 255, 98],
     [1, 13, 13, 13, 13, 13, 13, 255, 255, 255, 255, 13, 255, 255, 255, 255, 13, 13, 13, 255, 255, 13, 255, 255, 255, 255, 13, 255, 255, 255, 255, 13, 13, 13, 13, 13]
 ])
+valores = []
+letra = ''
 
 yd = np.array([0, 1])
 
-# Learning rate and maximum number of epochs
+# Tasa de aprendizaje y número máximo de épocas.
 alfa = 0.004
 max_epochs = 1000
 
-# Store weight values and errors for each epoch
+# Almacenar valores de peso y errores para cada época.
 weight_history = np.zeros((max_epochs, 36))
 error_history = np.zeros((max_epochs, 2))
 
-# Training loop
+# Bucle de entrenamiento
 for epoch in range(max_epochs):
     error_sum = np.zeros(2)
     for i in range(2):
         tsum = np.dot(w, x[i, :])
-        y0 = (tsum > 0).astype(int)  # Threshold activation function
+        y0 = (tsum > 0).astype(int)  # Función de activación de umbral
         error = yd[i] - y0
         delta_w = alfa * error * x[i, :]
         w = w + delta_w
         error_sum += np.abs(error)
     
-    # Store weight values and errors for this epoch
+    # Almacenar valores de peso y errores para esta época.
     weight_history[epoch, :] = w
     error_history[epoch, :] = error_sum
     
-    # Check for convergence (error is zero)
+    # Verifique la convergencia (el error es cero)
     if np.all(error_sum == 0):
         break
 
@@ -41,23 +44,73 @@ print('Epochs:', epoch + 1)
 print('Final Weights:')
 print(w)
 
-# Plot the weight values versus epoch
+#ventana para 
+def obtener_valores():
+    for i in range(35):
+        valor = float(cajas_texto[i].get())
+        valores.append(valor)
+    print("Valores ingresados por el usuario:", valores)
+    
+    # Cerrar la ventana actual
+    ventana.destroy()
+    
+# Crear la ventana principal
+ventana = tk.Tk()
+ventana.title("Ingresar Valores")
+
+# Crear 35 cajas de texto y etiquetas
+cajas_texto = []
+
+for i in range(7):
+    for j in range(5):
+        caja_texto = tk.Entry(ventana)
+        caja_texto.grid(row=i, column=j, padx=2, pady=2)
+        
+
+        cajas_texto.append(caja_texto)
+
+# Botón para obtener los valores ingresados
+boton_obtener = tk.Button(ventana, text="Obtener Valores", command=obtener_valores)
+boton_obtener.grid(row=7, column=0, columnspan=2, pady=10)
+
+# Iniciar el bucle de la ventana
+ventana.mainloop()
+
+valores.insert(0, 1)
+comp = np.dot(valores, w)
+if comp > 0:
+    letra = 'E'
+else:
+    letra = 'A'
+
+print (letra)
+    
+# Crear una nueva ventana para mostrar el valor de 'letra'
+nueva_ventana = tk.Tk()
+nueva_ventana.title("Resultado de la comprobacion")
+
+etiqueta_letra = tk.Label(nueva_ventana, text=f"La letra ingresada es: {letra}", font=("Arial", 80))
+etiqueta_letra.pack(padx=10, pady=5)
+
+nueva_ventana.mainloop()
+
+# Después de que la ventana tkinter se cierre, mostrar las gráficas
+# Trazar los valores de peso versus la época.
 plt.figure()
 for i in range(36):
     plt.plot(range(epoch + 1), weight_history[:epoch + 1, i])
-plt.xlabel('Epoch')
-plt.ylabel('Weight Value')
-plt.title('Weight Values vs. Epoch')
+plt.xlabel('época')
+plt.ylabel('valores de peso')
+plt.title('valores de peso vs. época')
 plt.grid(True)
 
-# Plot the error versus epoch
+# Trazar el error versus la época
 plt.figure()
 for i in range(2):
     plt.plot(range(epoch + 1), error_history[:epoch + 1, i], '-o', label=f'Output {i}')
-plt.xlabel('Epoch')
-plt.ylabel('Error Sum')
-plt.title('Error Sum vs. Epoch')
+plt.xlabel('época')
+plt.ylabel('Error')
+plt.title('Error vs. época')
 plt.legend()
 plt.grid(True)
-
 plt.show()
---
+++ Vocales.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+# Función de activación sigmoide
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# Derivada de la función de activación sigmoide
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+# Configuración de la RNA
+input_size = 35
+hidden_size = 10
+output_size = 3
+learning_rate = 0.1
+epochs = 10000
+
+# Datos de entrenamiento
+# Aquí debes proporcionar tus propios datos de entrenamiento
+# Cada fila representa una entrada de 35 dimensiones y su correspondiente salida de 3 dimensiones.
+# Asegúrate de que los datos estén en formato numpy array.
+X = np.array([
+    [0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0],
+    [1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0]
+])
+
+Y = np.array([
+    # Salida correspondiente al Patrón 1
+    [1, 0, 0],
+    # Salida correspondiente al Patrón 2
+    [0, 1, 0],
+    # Salida correspondiente al Patrón 3
+    [0, 0, 1],
+    # Salida correspondiente al Patrón 4
+    [1, 1, 0],
+    # Salida correspondiente al Patrón 5
+    [0, 1, 1],
+])
+
+# Inicialización de los pesos de forma aleatoria
+np.random.seed(1)
+weights_input_hidden = np.random.uniform(size=(input_size, hidden_size))
+weights_hidden_output = np.random.uniform(size=(hidden_size, output_size))
+
+# Entrenamiento de la RNA
+for epoch in range(epochs):
+    # Propagación hacia adelante
+    hidden_layer_input = np.dot(X, weights_input_hidden)
+    hidden_layer_output = sigmoid(hidden_layer_input)
+    output_layer_input = np.dot(hidden_layer_output, weights_hidden_output)
+    output_layer_output = sigmoid(output_layer_input)
+
+    # Cálculo del error
+    error = Y - output_layer_output
+
+    # Retropropagación del error
+    d_output = error * sigmoid_derivative(output_layer_output)
+    error_hidden_layer = d_output.dot(weights_hidden_output.T)
+    d_hidden_layer = error_hidden_layer * sigmoid_derivative(hidden_layer_output)
+
+    # Actualización de los pesos
+    weights_hidden_output += hidden_layer_output.T.dot(d_output) * learning_rate
+    weights_input_hidden += X.T.dot(d_hidden_layer) * learning_rate
+
+    # Verifique la convergencia (el error es cero)
+    if np.all(d_output == 0):
+        print("finaliza el entrenamientoA")
+        break
+print("finaliza el entrenamientoB")
+
+# Predicción
+new_input = np.array([1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0])  # Reemplaza con tus datos de entrada
+hidden_layer_input = np.dot(new_input, weights_input_hidden)
+hidden_layer_output = sigmoid(hidden_layer_input)
+output_layer_input = np.dot(hidden_layer_output, weights_hidden_output)
+predicted_output = sigmoid(output_layer_input)
+
+valores_redondeados = [round(valor, 0) for valor in predicted_output]
+
+# Imprimir los valores redondeados
+print(valores_redondeados)
