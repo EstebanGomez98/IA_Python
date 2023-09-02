bbfb1db54c6762bbc7dd71d0a850285508b39b5b
Juan Sebastian Quintero <strangelife28@gmail.com>
2023-09-01 20:17:36 -0500

    test2

--- TestInterface.py
+++ TestInterface.py
@@ -1,54 +1,70 @@
-from tkinter import Tk, filedialog, Button
-import tkinter as tk
+import PySimpleGUI as sg
 import numpy as np
 from PIL import Image
+import random
 
-# ... (rest of your code)
+# Initialize variables for training
+w = np.random.rand(36)
+xr = []
+yd = []
 
-# Function to train the network
+# Define the layout for the GUI
+layout = [
+    [sg.Text("Select an image and train the network")],
+    [sg.Button("Train Network"), sg.Button("Select Image"), sg.Button("Exit")],
+    [sg.Text("", size=(30, 1), key="-OUTPUT-")],
+]
+
+# Create the window
+window = sg.Window("Simple GUI for Training", layout)
+
+# Training function
 def train_network():
-    # Add your training code here
-    # For example:
-    # Training code can go here, similar to the training code in your __main__ block
-    pass
+    global w, xr, yd
+    # Replace this with your training code
+    # For now, let's just generate random data
+    w = np.random.rand(36)
+    xr = [np.random.rand(36) for _ in range(10)]
+    yd = [random.choice([0, 1]) for _ in range(10)]
+    sg.popup("Training completed!")
 
-# Function to scan an image and make a prediction
-def scan_image():
-    imgInput = select_and_resize_image()
-    imgInput.insert(0, 1)
-    comp = np.dot(imgInput, w)
-    if comp > 0:
-        letra = 'A'
+# Image selection function
+def select_image():
+    root = sg.tk.Tk()
+    root.withdraw()
+
+    file_path = sg.popup_get_file("Select an image file", file_types=(("Image Files", "*.png *.jpg *.jpeg *.gif *.bmp"),))
+
+    if file_path:
+        try:
+            original_image = Image.open(file_path)
+            resized_image = original_image.resize((5, 7))
+            resized_image = resized_image.convert("L")
+            pixel_data = list(resized_image.getdata())
+            return pixel_data
+        except Exception as e:
+            sg.popup_error("Error processing the image.")
+            return np.zeros(35)
     else:
-        letra = 'E'
+        sg.popup("No file selected.")
 
-    print(letra)
+while True:
+    event, values = window.read()
 
-    # Create a new tkinter window to display the result
-    result_window = Tk()
-    result_window.title("Result of the Scan")
+    if event == sg.WINDOW_CLOSED or event == "Exit":
+        break
+    elif event == "Train Network":
+        train_network()
+    elif event == "Select Image":
+        img_data = select_image()
+        if img_data:
+            img_data.insert(0, 1)
+            comp = np.dot(img_data, w)
+            if comp > 0:
+                letter = 'A'
+            else:
+                letter = 'E'
+            window["-OUTPUT-"].update(f"Predicted Letter: {letter}")
 
-    label_result = tk.Label(result_window, text=f"The scanned letter is: {letra}", font=("Arial", 80))
-    label_result.pack(padx=10, pady=5)
-    result_window.mainloop()
-
-# Function to close the system
-def close_system():
-    root.quit()
-
-# Create the main tkinter window
-root = Tk()
-root.withdraw()  # Hide the main window
-
-# Create buttons for training, scanning, and closing
-train_button = Button(root, text="Train Network", command=train_network)
-scan_button = Button(root, text="Scan Image", command=scan_image)
-close_button = Button(root, text="Close System", command=close_system)
-
-# Pack the buttons in the main window
-train_button.pack()
-scan_button.pack()
-close_button.pack()
-
-# Run the tkinter main loop
-root.mainloop()
+# Close the window
+window.close()
