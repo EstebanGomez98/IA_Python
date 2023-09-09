522360f9acea88c9a4984b7452922968c894648c
EstebanGomez98 <43140553+EstebanGomez98@users.noreply.github.com>
2023-09-09 16:39:28 -0500

    se agrega la graficacio del error

--- RNA/adaline/Base.py
+++ RNA/adaline/Base.py
@@ -6,9 +6,11 @@ font_size = 14  # Puedes ajustar el tamaño de la fuente aquí
 
 # Define the layout for the GUI
 layout = [
-    [sg.Text("Click 'Entrenar' para entrenar el modelo", font=("AnyFont", font_size))],
+    [sg.Text("Click 'Entrenar' para entrenar el modelo",
+             font=("AnyFont", font_size))],
     [sg.Button("Entrenar", font=("AnyFont", font_size))],
-    [sg.Text("Click 'Reconocer' para reconocer los bits", font=("AnyFont", font_size))],
+    [sg.Text("Click 'Reconocer' para reconocer los bits",
+             font=("AnyFont", font_size))],
     [sg.Button("Reconocer", font=("AnyFont", font_size))],
     [sg.Button("Finalizar", font=("AnyFont", font_size))]
 ]
@@ -22,7 +24,7 @@ def inicializar_pesos(num_entradas):
 # Función de entrenamiento
 
 
-def entrenar_perceptron(X, YD, alpha, precision):
+def entrenar_adaline(X, YD, alpha, precision):
     num_entradas = X.shape[1]
     num_muestras = X.shape[0]
 
@@ -34,6 +36,9 @@ def entrenar_perceptron(X, YD, alpha, precision):
 
     # Inicializar error anterior
     error_anterior = float('inf')
+
+    # Inicializar lista para almacenar errores por época
+    errores_por_epoca = []
 
     while True:
         # Inicializar el error para esta época
@@ -61,15 +66,18 @@ def entrenar_perceptron(X, YD, alpha, precision):
         # Calcular E(actual)
         error_actual /= num_muestras
 
+        # Almacenar el error en la lista de errores por época
+        errores_por_epoca.append(error_actual)
+
         # Imprimir información de la época actual
-        print(f"Época {epocas}: Error = {error_actual}")         
-        
+        print(f"Época {epocas}: Error = {error_actual}")
+
         # Verificar si se ha alcanzado la precisión deseada
         if error_diff < precision:
-            print("Entrenamiento completado.")                  
+            print("Entrenamiento completado.")
             break
-            
-    return W
+
+    return W, errores_por_epoca
 
 # Función para analizar datos utilizando el perceptrón entrenado
 
@@ -129,26 +137,26 @@ if __name__ == "__main__":
     # Parámetros de entrenamiento
     alpha = 0.1
     precision = 0.001
-    
+
     window = sg.Window("Adelaine Convertidor Binario a Decimal", layout)
 
     # Event loop
     while True:
         event, values = window.read()
-        
+
         if event == sg.WINDOW_CLOSED or event == "Finalizar":
             break
-        
+
         if event == "Entrenar":
             train_options = ["2 bit", "3 bit", "4 bit", "No entrenar"]
-            #choice = sg.popup("Seleccione la opcion de entrenamiento", custom_text=(train_options[0], train_options[1], train_options[2], train_options[3]))
+            # choice = sg.popup("Seleccione la opcion de entrenamiento", custom_text=(train_options[0], train_options[1], train_options[2], train_options[3]))
             layout_options = [
                 [sg.Text("Seleccione una opcion:")],
                 [sg.Radio("2 bit", "Bits", default=True, key="Option1")],
                 [sg.Radio("3 bit", "Bits", key="Option2")],
                 [sg.Radio("4 bit", "Bits", key="Option3")],
                 [sg.Button("Seleccionar"), sg.Button("Cerrar")]
-                ]
+            ]
 
 # Create the window
             window_options = sg.Window("Select an Option", layout_options)
@@ -164,26 +172,37 @@ if __name__ == "__main__":
                     for i in range(1, 5):
                         if values[f"Option{i}"]:
                             selected_option = f"Option{i}"
-                            break                   
+                            break
                     sg.popup(f"Selecciono {selected_option}")
             # Close the window
             window_options.close()
-        
+
             if selected_option:
                 if selected_option == "Option1":
-                    pesos_entrenados = entrenar_perceptron(X, YD, alpha, precision)
+                    pesos_entrenados, errores_por_epoca = entrenar_adaline(
+                        X, YD, alpha, precision)
                     global bits
-                    bits = 2 
+                    bits = 2
                 elif selected_option == "Option2":
-                    pesos_entrenados = entrenar_perceptron(X2, YD2, alpha, precision)
+                    pesos_entrenados, errores_por_epoca = entrenar_adaline(
+                        X2, YD2, alpha, precision)
                     bits = 3
                 elif selected_option == "Option3":
-                    pesos_entrenados = entrenar_perceptron(X3, YD3, alpha, precision)
+                    pesos_entrenados, errores_por_epoca = entrenar_adaline(
+                        X3, YD3, alpha, precision)
                     bits = 4
+
             sg.popup(f"Entrenamiento con '{selected_option}' completado.")
-        
+            # Graficar el error por época
+            plt.plot(range(1, len(errores_por_epoca) + 1), errores_por_epoca)
+            plt.xlabel("Época")
+            plt.ylabel("Error")
+            plt.title("Error por Época")
+            plt.grid(True)
+            plt.show()  # Mostrar la gráfica
+
         elif event == "Reconocer":
-            
+
             input_layout2 = [
                 [sg.Text("Digite los bits:")],
                 [sg.InputText(), sg.InputText()],
@@ -205,27 +224,33 @@ if __name__ == "__main__":
                 input_window = sg.Window("Digite bit por bit", input_layout3)
             else:
                 input_window = sg.Window("Digite bit por bit", input_layout4)
-            
+
             while True:
                 input_event, input_values = input_window.read()
                 if input_event == sg.WINDOW_CLOSED or input_event == "OK":
                     break
             input_window.close()
-            
+
             if input_event == "OK":
-                
+
                 # Convert the entered numbers to an array
                 if bits == 2:
-                    num_array = np.array([[float(input_values[0]), float(input_values[1])]])
+                    num_array = np.array(
+                        [[float(input_values[0]), float(input_values[1])]])
                 elif bits == 3:
-                    num_array = np.array([[float(input_values[0]), float(input_values[1]), float(input_values[2])]])
+                    num_array = np.array(
+                        [[float(input_values[0]), float(input_values[1]), float(input_values[2])]])
                 else:
-                    num_array = np.array([[float(input_values[0]), float(input_values[1]), float(input_values[2]) , float(input_values[3])]])
-                
+                    num_array = np.array([[float(input_values[0]), float(
+                        input_values[1]), float(input_values[2]), float(input_values[3])]])
+
                 # Perform recognition using the entered numbers (replace with your code)
-                resultados_analisis = analizar_datos(num_array, pesos_entrenados)
-                valores_redondeados = [round(valor, 0) for valor in resultados_analisis]
-                sg.popup(f"Reconocimiento completado, el resultado es: ", valores_redondeados)
+                resultados_analisis = analizar_datos(
+                    num_array, pesos_entrenados)
+                valores_redondeados = [round(valor, 0)
+                                       for valor in resultados_analisis]
+                sg.popup(f"Reconocimiento completado, el resultado es: ",
+                         valores_redondeados)
 
     # Close the window
     window.close()
