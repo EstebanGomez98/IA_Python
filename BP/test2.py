0c0cdc7aba124308e60d5bbaa52dc409e21c6492
Juan Sebastian Quintero <juansquintero28@gmail.com>
2023-09-24 18:56:16 -0500

    test2

--- BP/test2.py
+++ BP/test2.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pandas as pd
 
-# Define the sigmoid activation function and its derivative
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+# Define the ReLU activation function and its derivative
+def relu(x):
+    return np.maximum(0, x)
 
-def sigmoid_derivative(x):
-    return x * (1 - x)
+def relu_derivative(x):
+    return np.where(x > 0, 1, 0)
 
 # Load your data (replace the path with your data file)
 excel_file = r"D:\Windows Lib\Documents\Repos\IA_Python\BP\data.xlsx"
@@ -17,7 +17,7 @@ last_column = df.iloc[:, -1].to_numpy()
 array_of_arrays = last_column.reshape(-1, 1)
 
 # Normalize the input data
-X = (selected_columns - selected_columns.mean(axis=0)) / selected_columns.std(axis=0)
+X = selected_columns
 
 # Outputs
 y = array_of_arrays
@@ -28,34 +28,34 @@ hidden_size = 40
 output_size = 1
 
 # Hyperparameters
-learning_rate = 0.01  # Adjust the learning rate if needed
+learning_rate = 0.001
 momentum = 0.09
 
-# Initialize weights with random initialization
+# Initialize weights with random initialization close to zero
 np.random.seed(1)
-weights_input_hidden = np.random.randn(input_size, hidden_size)
-weights_hidden_output = np.random.randn(hidden_size, output_size)
+weights_input_hidden = np.random.randn(input_size, hidden_size) * 0.01
+weights_hidden_output = np.random.randn(hidden_size, output_size) * 0.01
 
 # Initialize momentum terms
 momentum_input_hidden = np.zeros_like(weights_input_hidden)
 momentum_hidden_output = np.zeros_like(weights_hidden_output)
 
 # Training the neural network
-num_epochs = 1000  # Adjust the number of epochs
+num_epochs = 1000
 for epoch in range(num_epochs):
-    # Forward propagation
+    # Forward propagation with ReLU activation
     hidden_layer_input = np.dot(X, weights_input_hidden)
-    hidden_layer_output = sigmoid(hidden_layer_input)
+    hidden_layer_output = relu(hidden_layer_input)
     output_layer_input = np.dot(hidden_layer_output, weights_hidden_output)
-    output_layer_output = sigmoid(output_layer_input)
+    output_layer_output = relu(output_layer_input)
 
     # Calculate the loss
     error = y - output_layer_output
 
-    # Backpropagation
-    d_output = error * sigmoid_derivative(output_layer_output)
+    # Backpropagation with ReLU derivative
+    d_output = error * relu_derivative(output_layer_output)
     error_hidden_layer = d_output.dot(weights_hidden_output.T)
-    d_hidden_layer = error_hidden_layer * sigmoid_derivative(hidden_layer_output)
+    d_hidden_layer = error_hidden_layer * relu_derivative(hidden_layer_input)
 
     # Update weights and biases with momentum
     momentum_input_hidden = (
@@ -75,9 +75,9 @@ for epoch in range(num_epochs):
 for i in range(10):
     new_input = X[i, :]
     hidden_layer_input = np.dot(new_input, weights_input_hidden)
-    hidden_layer_output = sigmoid(hidden_layer_input)
+    hidden_layer_output = relu(hidden_layer_input)
     output_layer_input = np.dot(hidden_layer_output, weights_hidden_output)
-    predicted_output = sigmoid(output_layer_input)
+    predicted_output = relu(output_layer_input)
 
     print(f"Input {i + 1}: {new_input}")
     print("-----")
