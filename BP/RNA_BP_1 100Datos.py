f9850503809bd79463a5e2ace8964d1bdffb7c0e
Juan Sebastian Quintero <strangelife28@gmail.com>
2023-09-24 17:21:03 -0500

    cosas esas

--- BP/RNA_BP_1 100Datos.py
+++ BP/RNA_BP_1 100Datos.py
@@ -1,62 +1,39 @@
 import numpy as np
+import pandas as pd
 
 # Define the sigmoid activation function and its derivative
-
-
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
-
 
 def sigmoid_derivative(x):
     return x * (1 - x)
 
-
 # Define the training data
-X = np.array([[0.552841309, 0.547656981191987, -1],
-              [0.36226323, 0.568600319, 1],
-              [0, 0, 1],
-              [0, 0, 1],
-              [0, 1, 0],
-              [0, 1, 0],
-              [0, 1, 1],
-              [0, 1, 1],
-              [1, 0, 0],
-              [1, 0, 0],
-              [1, 0, 1],
-              [1, 0, 1],
-              [1, 1, 0],
-              [1, 1, 0],
-              [1, 1, 1],
-              [1, 1, 1]])
 
-# Define the corresponding labels
-y = np.array([[0, 378854248],
-              [1],
-              [1],
-              [0],
-              [1],
-              [0],
-              [0],
-              [1],
-              [1],
-              [0],
-              [0],
-              [1],
-              [0],
-              [1],
-              [1],
-              [0]])
+# Read the Excel file into a pandas DataFrame
+excel_file = r"C:\Users\stran\OneDrive\Documents\Repos\IA_Python\BP\data.xlsx"  # Replace with the correct path
+df = pd.read_excel(excel_file)
+df.iloc[:, 2] = df.iloc[:, 2].astype(int).astype(str).astype(int)
+selected_columns = df.iloc[:, :3].to_numpy()
+last_column = df.iloc[:, -1].to_numpy()
+array_of_arrays = last_column.reshape(-1, 1)
+# Entradas
+X = selected_columns
+print("Entradas",selected_columns)
 
+# Salidas
+y = array_of_arrays  # Use the reshaped array as the target variable
+print(array_of_arrays)
 # Initialize the neural network parameters
-input_size = 4
-hidden_size = 3
+input_size = 3
+hidden_size = 20
 output_size = 1
 
 # Alpha
-learning_rate = 0.001
+learning_rate = 0.01  # Adjust the learning rate if needed
 
-# Beta
-beta = 0.9
+# Beta (momentum)
+beta = 0.09
 
 # Initialize weights and biases
 np.random.seed(1)
@@ -81,8 +58,7 @@ for epoch in range(10000):
     # Backpropagation
     d_output = error * sigmoid_derivative(output_layer_output)
     error_hidden_layer = d_output.dot(weights_hidden_output.T)
-    d_hidden_layer = error_hidden_layer * \
-        sigmoid_derivative(hidden_layer_output)
+    d_hidden_layer = error_hidden_layer * sigmoid_derivative(hidden_layer_output)
 
     # Update weights and biases with momentum
     momentum_input_hidden = (
@@ -94,7 +70,7 @@ for epoch in range(10000):
     weights_hidden_output += momentum_hidden_output
 
 # Testing the neural network
-new_input = np.array([0, 1, 0, 0])
+new_input = np.array([0.125693786272204, 0.261253789224246, 1.0])
 hidden_layer_input = np.dot(new_input, weights_input_hidden)
 hidden_layer_output = sigmoid(hidden_layer_input)
 output_layer_input = np.dot(hidden_layer_output, weights_hidden_output)
