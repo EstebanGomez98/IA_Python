5e63b72cea44311fd8a35b1cd8cc3f64db7e72dc
Juan Sebastian Quintero <juansquintero28@gmail.com>
2023-10-06 20:42:45 -0500

    asdasdsadfgwerasgaers

--- TEST/test2.py
+++ TEST/test2.py
@@ -1,44 +1,62 @@
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
-import os
 import numpy as np
+import cv2
 
 def aplicar_filtro(matriz, kernel):
-    altura, ancho = matriz.shape
+    m, n = matriz.shape
     kaltura, kancho = kernel.shape
-    resultado = np.zeros((altura - kaltura + 1, ancho - kancho + 1))
-    for i in range(altura - kaltura + 1):
-        for j in range(ancho - kancho + 1):
-            resultado[i, j] = np.sum(matriz[i:i+kaltura, j:j+kancho] * kernel)
+    kaltura_half, kancho_half = kaltura // 2, kancho // 2
+    
+    resultado = np.zeros((m, n), dtype=np.float32)
+    
+    for i in range(kaltura_half, m - kaltura_half):
+        for j in range(kancho_half, n - kancho_half):
+            suma = 0
+            for x in range(kaltura):
+                for y in range(kancho):
+                    suma += matriz[i - kaltura_half + x, j - kancho_half + y] * kernel[x, y]
+            resultado[i, j] = suma
+    
     return resultado
 
-def mostrar_imagen_modificada(matriz):
-    # Create a new Image object from the modified matrix
-    imagen_modificada = Image.fromarray(matriz.astype('uint8'))
-
-    # Convert the modified image to PhotoImage format for Tkinter
-    imagen_tk = ImageTk.PhotoImage(imagen_modificada)
+def mostrar_imagen_modificada(imagen):
+    # Convert the modified image to RGB mode
+    imagen_rgb = imagen.convert("RGB")
+    
+    # Convert the RGB image to PhotoImage format for Tkinter
+    imagen_tk = ImageTk.PhotoImage(imagen_rgb)
 
     # Update the label to display the modified image
     imagen_label.config(image=imagen_tk)
     imagen_label.image = imagen_tk
+
+def guardar_imagen_modificada(imagen):
+    # Ask the user for the save file location
+    ruta_guardar = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
+
+    if ruta_guardar:
+        try:
+            # Save the modified image
+            imagen.save(ruta_guardar)
+            resultado_label.config(text='Imagen guardada como ' + ruta_guardar)
+        except Exception as e:
+            resultado_label.config(text='Error al guardar la imagen: ' + str(e))
 
 def cambiar_tamano_imagen():
     # Solicitar al usuario que seleccione una imagen
     ruta_imagen = filedialog.askopenfilename()
 
     k1 = np.array([
-        [1, 0, -1],
-        [1, 0, -1],
-        [1, 0, -1]])
+    [1, 0, -1],
+    [1, 0, -1],
+    [1, 0, -1]])
 
     k2 = np.array([
-        [0, 0, 0],
-        [1, 1, 1],
-        [0, 0, 0]])
-
-    global matriz_red
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0]])
 
     if ruta_imagen:
         try:
@@ -53,22 +71,33 @@ def cambiar_tamano_imagen():
 
             # Aplicar los filtros k1 y k2
             resultado_k1 = aplicar_filtro(matriz_red, k1)
-            resultado_k2 = aplicar_filtro(matriz_red, k2)
+            resultado_k2 = aplicar_filtro(resultado_k1, k2)
+
+            # Convert the result back to an Image object
+            imagen_modificada = Image.fromarray(resultado_k2.astype('uint8'))
 
             # Mostrar la imagen modificada después de aplicar los filtros
-            mostrar_imagen_modificada(resultado_k1)  # You can choose either k1 or k2 here.
+            mostrar_imagen_modificada(imagen_modificada)
+
+            # Enable the guardar_boton button
+            guardar_boton.config(state=tk.NORMAL, command=lambda: guardar_imagen_modificada(imagen_modificada))
 
         except Exception as e:
             resultado_label.config(text='Error: ' + str(e))
 
 # Crear una ventana de tkinter
 ventana = tk.Tk()
-ventana.title('Redimensionar Imagen')
+ventana.title('Redimensionar y Filtrar Imagen')
 
 # Botón para seleccionar una imagen
 seleccionar_boton = tk.Button(
     ventana, text='Seleccionar Imagen', command=cambiar_tamano_imagen)
 seleccionar_boton.pack(pady=20)
+
+# Botón para guardar la imagen modificada
+guardar_boton = tk.Button(
+    ventana, text='Guardar Imagen Modificada', state=tk.DISABLED)
+guardar_boton.pack()
 
 # Etiqueta para mostrar el resultado
 resultado_label = tk.Label(ventana, text='')
