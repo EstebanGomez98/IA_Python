b35d9b2eb0fa48efcf61334c066cb38b480d9c13
EstebanGomez98 <43140553+EstebanGomez98@users.noreply.github.com>
2023-10-06 21:57:42 -0500

    se mejora la funcion de convolucion

--- TEST/test2.py
+++ TEST/test2.py
@@ -2,29 +2,50 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import numpy as np
-import cv2
+
 
 def aplicar_filtro(matriz, kernel):
     m, n = matriz.shape
     kaltura, kancho = kernel.shape
     kaltura_half, kancho_half = kaltura // 2, kancho // 2
-    
+
     resultado = np.zeros((m, n), dtype=np.float32)
-    
+
     for i in range(kaltura_half, m - kaltura_half):
         for j in range(kancho_half, n - kancho_half):
             suma = 0
             for x in range(kaltura):
                 for y in range(kancho):
-                    suma += matriz[i - kaltura_half + x, j - kancho_half + y] * kernel[x, y]
+                    suma += matriz[i - kaltura_half + x,
+                                   j - kancho_half + y] * kernel[x, y]
             resultado[i, j] = suma
-    
+
     return resultado
+
+
+def convolucion(imagen, kernel):
+    altura_imagen, ancho_imagen = len(imagen), len(imagen[0])
+    altura_kernel, ancho_kernel = len(kernel), len(kernel[0])
+    resultado = np.zeros((altura_imagen - altura_kernel + 1,
+                         ancho_imagen - ancho_kernel + 1), dtype=np.float32)
+
+    # Iterar a través de la imagen
+    for i in range(altura_imagen - altura_kernel + 1):
+        for j in range(ancho_imagen - ancho_kernel + 1):
+            suma = 0
+            # Realizar la convolución en la región de la imagen correspondiente al tamaño del kernel
+            for x in range(altura_kernel):
+                for y in range(ancho_kernel):
+                    suma += imagen[i + x][j + y] * kernel[x][y]
+            resultado[i][j] = suma
+
+    return resultado
+
 
 def mostrar_imagen_modificada(imagen):
     # Convert the modified image to RGB mode
     imagen_rgb = imagen.convert("RGB")
-    
+
     # Convert the RGB image to PhotoImage format for Tkinter
     imagen_tk = ImageTk.PhotoImage(imagen_rgb)
 
@@ -32,9 +53,11 @@ def mostrar_imagen_modificada(imagen):
     imagen_label.config(image=imagen_tk)
     imagen_label.image = imagen_tk
 
+
 def guardar_imagen_modificada(imagen):
     # Ask the user for the save file location
-    ruta_guardar = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
+    ruta_guardar = filedialog.asksaveasfilename(
+        defaultextension=".png", filetypes=[("PNG files", "*.png")])
 
     if ruta_guardar:
         try:
@@ -42,21 +65,22 @@ def guardar_imagen_modificada(imagen):
             imagen.save(ruta_guardar)
             resultado_label.config(text='Imagen guardada como ' + ruta_guardar)
         except Exception as e:
-            resultado_label.config(text='Error al guardar la imagen: ' + str(e))
+            resultado_label.config(
+                text='Error al guardar la imagen: ' + str(e))
+
 
 def cambiar_tamano_imagen():
-    # Solicitar al usuario que seleccione una imagen
-    ruta_imagen = filedialog.askopenfilename()
-
     k1 = np.array([
-    [1, 0, -1],
-    [1, 0, -1],
-    [1, 0, -1]])
+        [1, 0, -1],
+        [1, 0, -1],
+        [1, 0, -1]])
 
     k2 = np.array([
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 0, 0]])
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0]])
+    # Solicitar al usuario que seleccione una imagen
+    ruta_imagen = filedialog.askopenfilename()
 
     if ruta_imagen:
         try:
@@ -67,11 +91,11 @@ def cambiar_tamano_imagen():
             imagen_redimensionada = imagen.resize((24, 24))
 
             # Obtener la matriz de la imagen redimensionada
-            matriz_red = np.array(imagen_redimensionada.convert("L"))
+            matriz_img = np.array(imagen_redimensionada.convert("L"))
 
             # Aplicar los filtros k1 y k2
-            resultado_k1 = aplicar_filtro(matriz_red, k1)
-            resultado_k2 = aplicar_filtro(resultado_k1, k2)
+            resultado_k1 = convolucion(matriz_img, k1)
+            resultado_k2 = convolucion(resultado_k1, k2)
 
             # Convert the result back to an Image object
             imagen_modificada = Image.fromarray(resultado_k2.astype('uint8'))
@@ -80,19 +104,27 @@ def cambiar_tamano_imagen():
             mostrar_imagen_modificada(imagen_modificada)
 
             # Enable the guardar_boton button
-            guardar_boton.config(state=tk.NORMAL, command=lambda: guardar_imagen_modificada(imagen_modificada))
+            guardar_boton.config(
+                state=tk.NORMAL, command=lambda: guardar_imagen_modificada(imagen_modificada))
 
         except Exception as e:
             resultado_label.config(text='Error: ' + str(e))
+            print(e)
 
-# Crear una ventana de tkinter
+
+# Crear una ventana de tkinter con dimensiones personalizadas
 ventana = tk.Tk()
 ventana.title('Redimensionar y Filtrar Imagen')
+
+# Definir las dimensiones de la ventana (ancho x alto)
+dimensiones_ventana = "800x600"
+ventana.geometry(dimensiones_ventana)
 
 # Botón para seleccionar una imagen
 seleccionar_boton = tk.Button(
     ventana, text='Seleccionar Imagen', command=cambiar_tamano_imagen)
-seleccionar_boton.pack(pady=20)
+seleccionar_boton.pack(pady=50)
+
 
 # Botón para guardar la imagen modificada
 guardar_boton = tk.Button(
