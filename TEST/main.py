168aa775c2323e448ae736a04f79316cc9afeeb6
Juan Sebastian Quintero <juansquintero28@gmail.com>
2023-10-08 16:37:11 -0500

    a

--- TEST/main.py
+++ TEST/main.py
@@ -32,7 +32,7 @@ def mostrar_imagen_modificada(imagen):
     imagen_label.config(image=imagen_tk)
     imagen_label.image = imagen_tk
 
-def cambiar_tamano_imagen():
+def cambiar_tamano_imagen(ruta_imagen):
     k1 = np.array([
         [1, 0, -1],
         [1, 0, -1],
@@ -61,53 +61,19 @@ def cambiar_tamano_imagen():
         [1, -4, 1],
         [0, 1, 0]])
 
-    # Solicitar al usuario que seleccione una imagen
-    ruta_imagen = filedialog.askopenfilename()
-
     if ruta_imagen:
         try:
-            # Abrir la imagen
-            imagen = Image.open(ruta_imagen)
-
-            # Cambiar el tamaño de la imagen
-            # imagen_redimensionada = imagen.resize((800, 600))
-
-            # Obtener la matriz de la imagen redimensionada
-            matriz_img = np.array(imagen.convert("L"))
-
-            # Aplicar los filtros k1 y k2
-            print("inicando convoluciones con los filtros")
-            print("convolucion 1")
+            matriz_img = ruta_imagen
             resultado = convolucion(matriz_img, k5)
-            print("convolucion 2")
-            # resultado = convolucion(resultado, k2)
             resultado = convolucion(resultado, k3)
-            print("convolucion 3")
             resultado = convolucion(resultado, k4)
-            print("terminando las convoluciones")
-            # resultado = convolucion(resultado, k5)
-
-            # Convert the result back to an Image object
             imagen_modificada = Image.fromarray(resultado.astype('uint8'))
-
-            # Cambiar el tamaño de la imagen
             imagen_redimensionada = imagen_modificada.resize((20, 20))
-
-            # Mostrar la imagen modificada después de aplicar los filtros
-            mostrar_imagen_modificada(imagen_redimensionada)
-
-            # Enable the guardar_boton button
-            guardar_imagen_modificada.config(
-                state=tk.NORMAL, command=lambda: guardar_imagen_modificada(imagen_redimensionada))
+            img_array = np.array(imagen_redimensionada)
+            return img_array
 
         except Exception as e:
             resultado_label.config(text='Error: ' + str(e))
-            print(e)
-
-# Function to train
-# Function to train
-# Function to train
-
 
 def entrenar():
     # Define your input data and labels
@@ -177,7 +143,7 @@ def entrenar():
         input_size, hidden_size1, hidden_size2, output_size)
 
     errores = train_neural_network(X, yd, weights_input_hidden1, weights_hidden1_hidden2, weights_hidden2_output, alpha, beta, error_deseado)
-    
+
     resultado_label.config(text='Entrenamiento en progreso...')
 
 # Function to recognize image
