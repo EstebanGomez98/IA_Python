fa42f51d20e71b3f9d679333f38e29d5091f6188
Juan Sebastian Quintero <juansquintero28@gmail.com>
2023-10-08 20:36:44 -0500

    notese

--- TEST/test5.py
+++ TEST/test5.py
@@ -5,37 +5,47 @@ from PIL import Image, ImageTk
 import numpy as np
 from sklearn.neural_network import MLPClassifier
 import joblib
+import os
 
-# Función para cargar imágenes y preprocesarlas
+training_folder = "input_images"
+# Function to load and preprocess images
 def load_and_preprocess_image(file_path):
-    img = Image.open(file_path).convert('L')  # Abre la imagen y conviértela a escala de grises
-    img = img.resize((28, 28))  # Redimensiona la imagen a 28x28 píxeles
-    img = np.array(img)  # Convierte la imagen en una matriz numpy
-    img = img.flatten()  # Aplana la matriz en un vector de 784 elementos
-    img = img / 255.0  # Normaliza los valores de píxeles (escala de 0 a 1)
+    img = Image.open(file_path).convert('L')  
+    img = img.resize((28, 28))  
+    img = np.array(img)  
+    img = img.flatten()  
+    img = img / 255.0  
     return img
 
-# Función para entrenar una red neuronal y guardar el modelo
+# Function to train a neural network and save the model
 def train_neural_network(training_data, labels, model_filename):
-    clf = MLPClassifier(hidden_layer_sizes=(50,), max_iter=100, activation='relu', solver='adam',
-                        random_state=1)
+    clf = MLPClassifier(hidden_layer_sizes=(50,), max_iter=500, activation='relu', solver='adam', random_state=1)
     clf.fit(training_data, labels)
     joblib.dump(clf, model_filename)
     return clf
 
-# Función para cargar el modelo entrenado
+# Function to load a trained model
 def load_trained_model(model_filename):
     clf = joblib.load(model_filename)
     return clf
 
-# Función para predecir letras usando la red neuronal
+# Function to predict letters using the neural network
 def predict_letter(model, image):
     letter_mapping = {0: 'A', 1: 'E', 2: 'I', 3: 'O', 4: 'U'}
-    predicted_label = model.predict([image])
-    predicted_letter = letter_mapping[predicted_label[0]]
+    predicted_label = model.predict([image])[0]
+    
+    # Add debugging print statement
+    print(f"Predicted Label: {predicted_label}")
+    
+    if predicted_label in letter_mapping:
+        predicted_letter = letter_mapping[predicted_label]
+    else:
+        predicted_letter = predicted_label
+    
     return predicted_letter
 
-# Función para manejar la acción del botón "Cargar Imagen"
+
+# Function to handle the "Load Image" button action
 def load_image():
     file_path = filedialog.askopenfilename()
     if file_path:
@@ -46,33 +56,54 @@ def load_image():
         image_label.config(image=img)
         image_label.image = img
         letter = predict_letter(neural_network, image)
-        prediction_label.config(text=f"Letra predicha: {letter}")
+        prediction_label.config(text=f"Predicted Letter: {letter}")
 
-# Función para manejar la acción del botón "Entrenar Red Neuronal"
+def load_training_data():
+    training_data = []
+    labels = []
+
+    # Define the list of letters
+    letters = ['A', 'E', 'I', 'O', 'U']
+
+    # Load 5 images for each letter
+    for letter in letters:
+        for i in range(1, 6):  # Load A1, A2, A3, A4, A5 for 'A', and so on
+            image_path = os.path.join(training_folder, f"{letter}{i}.jpg")
+            if os.path.exists(image_path):
+                image = load_and_preprocess_image(image_path)
+                training_data.append(image)
+                labels.append(letter)
+
+    return training_data, labels
+
+# Function to handle the "Train Neural Network" button action
 def train_network():
-    training_data = []  # Debe llenarse con datos de entrenamiento
-    labels = []  # Debe llenarse con las etiquetas correspondientes
+    global neural_network
+    training_data, labels = load_training_data()
+    
+    if len(training_data) == 0 or len(labels) == 0:
+        messagebox.showerror("Error", "Training data and labels are empty.")
+        return
+
     neural_network = train_neural_network(training_data, labels, "neural_network_model.pkl")
-    messagebox.showinfo("Entrenamiento completado", "La red neuronal ha sido entrenada y el modelo se ha guardado.")
+    messagebox.showinfo("Training Completed", "The neural network has been trained and the model has been saved.")
 
-# Crear la ventana principal
+# Create the main window
 root = tk.Tk()
-root.title("Reconocimiento de Letras")
+root.title("Letter Recognition")
 
-# Crear la interfaz de usuario
-load_button = tk.Button(root, text="Cargar Imagen", command=load_image)
-train_button = tk.Button(root, text="Entrenar Red Neuronal", command=train_network)
+# Create the user interface elements
+load_button = tk.Button(root, text="Load Image", command=load_image)
+train_button = tk.Button(root, text="Train Neural Network", command=train_network)
 image_label = tk.Label(root)
-prediction_label = tk.Label(root, text="Letra predicha: ")
+prediction_label = tk.Label(root, text="Predicted Letter: ")
 
 load_button.pack()
 train_button.pack()
 image_label.pack()
 prediction_label.pack()
 
-# Cargar una red neuronal entrenada (si existe)
-neural_network = load_trained_model("neural_network_model.pkl")
+# Load a trained neural network (if it exists)
+neural_network = None
 
 root.mainloop()
-
-pip install tkinter numpy matplotlib pillow scikit-learn
