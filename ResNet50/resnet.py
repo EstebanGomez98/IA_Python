fc7a9d52ae7e3b7ce377f1484c15326e4dc450bb
EstebanGomez98 <43140553+EstebanGomez98@users.noreply.github.com>
2023-11-02 14:50:19 -0500

    perro loco

--- ResNet50/resnet.py
+++ ResNet50/resnet.py
@@ -18,10 +18,13 @@ preprocess = transforms.Compose([
     transforms.Resize(400),  # Increase the image size
     transforms.CenterCrop(400),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]),
 ])
 
 # Function to perform image recognition
+
+
 def recognize_image():
     file_path = os.path.abspath(filedialog.askopenfilename())
     if file_path:
@@ -38,12 +41,14 @@ def recognize_image():
 
         # Get the top 5 predicted classes and their probabilities
         top5_prob, top5_catid = torch.topk(probabilities, 5)
-        class_labels = [class_names[top5_catid[i]] for i in range(top5_prob.size(0))]
+        class_labels = [class_names[top5_catid[i]]
+                        for i in range(top5_prob.size(0))]
 
         # Display the recognition results
         results_label.config(text="\nTop 5 clases mas reconocidas:")
         for i in range(top5_prob.size(0)):
-            results_label.config(text=results_label.cget("text") + f"\n{class_labels[i]}: {top5_prob[i].item() * 100:.2f}%")
+            results_label.config(text=results_label.cget(
+                "text") + f"\n{class_labels[i]}: {top5_prob[i].item() * 100:.2f}%")
 
         # Display the image
         img = Image.open(file_path)
@@ -52,6 +57,7 @@ def recognize_image():
         img_label.config(image=img)
         img_label.image = img
 
+
 # Create the main GUI window
 root = tk.Tk()
 root.title("ResNet Reconocimiento de imagenes")
@@ -59,7 +65,8 @@ root.geometry("800x600")  # Set window size
 root.option_add('*Font', 'Helvetica 14 bold')  # Set a bold font
 
 # Button to trigger image recognition
-recognize_button = Button(root, text="RECONOCER IMAGEN", command=recognize_image)
+recognize_button = Button(
+    root, text="RECONOCER IMAGEN", command=recognize_image)
 recognize_button.pack()
 
 # Label for recognition results
@@ -71,7 +78,7 @@ img_label = Label(root)
 img_label.pack()
 
 # Load the ImageNet class labels
-with open("imagenet-classes.txt") as f:
+with open("D:\Biblioteca\Documentos\IA_Python\ResNet50\imagenet-classes.txt") as f:
     class_names = [line.strip() for line in f.readlines()]
 
 # Start the GUI main loop
